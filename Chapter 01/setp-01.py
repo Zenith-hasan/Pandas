d586f3286348889ac2b1f5c8ee81a394085d707e
Zenith-hasan <abuhasan323382@gmail.com>
2025-05-01 09:37:59 +0600

    basic discusstion on Series

--- Chapter 01/setp-01.py
+++ Chapter 01/setp-01.py
@@ -2,3 +2,16 @@
 #? The Journey to learn about the pandas start!
 
 print("Let's Learn Pandas!")
+
+#? pandas is a powerful,open-source  data analysis and minipulation libary for python build top of the Numpy Package . Pandas is derived from the the word panel data.
+ 
+
+
+#? Series in Pandas 
+
+# In pandas, a Series is a one-dimentional labeled array capable of holding any data type -integers,strings,floats,python objects etc. It's similar to a column in a spreadsheet or a single column in a dataframe 
+
+#? Data Frame in Pandas
+
+#In pandas, a DataFame ins a twon dimentional, labeled data structure - like a table or a spreadsheet - with rows and columns. It is one of the most commonly used data structures in data analysis and manipulation tasks.
+
